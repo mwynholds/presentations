b7475b3d7e8755420144feb435a10e837b5b94ea
Michael Wynholds <mike@carbonfive.com>
2013-07-17 16:17:31 -0700

    Abouth halfway done with this talk

---
+++ README.md
@@ -0,0 +1,11 @@
+## The Startup Developer Toolkit
+
+This is the repo for a talk I give to various startup accelerators
+around the LA area.
+
+View these slides:
+<http://carbonfive.github.io/startup-developer-toolkit>  
+
+To run these slides locally, clone the repo and run:
+
+    ./serve.sh
---
+++ slide_config.js
@@ -0,0 +1,27 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'The Startup Developer Toolkit',
+    subtitle: 'eight simple tools that all developers should use',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/carbonfive-logo.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Michael Wynholds',
+    company: 'Carbon Five',
+    twitter: '@mwynholds, @carbonfive',
+    www: 'http://www.carbonfive.com',
+    github: 'http://github.com/carbonfive'
+  }]
+};
+
